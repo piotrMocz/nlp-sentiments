588ad94c70dab98863f3e1e01f61eb004fb5ded2
Michał Adamczyk <michau.adamczyk@gmail.com>
2016-04-27 09:21:03 +0200

    Add new constants to config.

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
-__author__ = 'Michal'
-
-
 
 DIR_DATABASE = "/database/"
 DIR_FILES = "/train_data/training_set"
 DIR_CLEANED_FILES = "./cleaned_files/"
 DIR_DUMP = "./dump/"
+DIR_NEWS = "./news/"
+DIR_CLEANED_NEWS = "./cleaned_news/"
