3b27cba43abd4cebd56b9faefba0403a76c2e582
piotrMocz <piotr.moczurad@gmail.com>
2016-05-11 21:54:50 +0200

    Constructing networkx graph from the ngrams

--- graph/graph_builder.py
+++ graph/graph_builder.py
@@ -1,67 +1,30 @@
 import os
+import cPickle
+
+import networkx as nx
+
 import config
-import file_utils
+import ngram_builder as ng
 
 
-def make_ngram_strings(fname, n):
+def make_graph():
     """
-    Returns list of ngrams (as strings) for a given file.
-    Strings are used to enable dict-based storage.
-    :param fname: name of the file to read words from
-    :param n: the rank of ngrams to create
-    :return: dictionary: {ngram -> num of occurrences}
+    Makes word order graph from the serialized edge dict
+    :return: graph with connections representing words occurring one after another
     """
-    words = file_utils.get_words_from_file(fname, as_set=False)
+    G = nx.DiGraph()
 
-    wcount = len(words)
-    ngrams = {}
+    with open(os.path.join(config.DIR_DUMP, 'dump_edges.pkl'), 'rb') as f:
+        digrams = cPickle.load(f)
 
-    for i in xrange(wcount - n + 1):
-        ngram = ' '.join(words[i:i+n])
+        for digram_str, cnt in digrams.iteritems():
+            w1, w2 = digram_str.split(' ')
+            G.add_edge(w1, w2)
 
-        if ngram in ngrams:
-            ngrams[ngram] += 1
-        else:
-            ngrams[ngram] = 1
-
-    return ngrams
-
-
-def combine_ngrams_dicts(dict1, dict2):
-    """"
-    Combines two dictionaries with ngrams counts into one
-    :param dict1 First dict
-    :param dict2 Second dict
-    :return dict with combined occurrence counts for ngrams
-    """
-    for ngram, cnt in dict2.iteritems():
-        if ngram in dict1:
-            dict1[ngram] += cnt
-        else:
-            dict1[ngram] = cnt
-
-    return dict1
-
-
-def get_all_ngrams(file_dir, n):
-    """
-    Returns all ngrams from files from a given directory
-    :param file_dir: directory to read files from
-    :param n: rank of ngrams to make
-    :return: string-based dictionary of all ngrams in the directory
-    """
-    files = (os.path.join(file_dir, f) for f in os.listdir(file_dir) if os.path.isfile(os.path.join(file_dir, f)))
-    ngram_dicts = (make_ngram_strings(f, n) for f in files)
-    ngrams_dict = {}
-
-    for ngd in ngram_dicts:
-        ngrams_dict = combine_ngrams_dicts(ngd, ngrams_dict)
-
-    return ngrams_dict
+        return G
 
 
 if __name__ == '__main__':
-    f_dir = os.path.join('..', config.DIR_CLEANED_FILES)
-    ngd = get_all_ngrams(f_dir, 2)
-    for k, v in ngd.iteritems():
-        print "{0}: {1}".format(k, v)
+    G = make_graph()
+    print len(G.nodes())
+    print len(G.edges())
---
+++ graph/ngram_builder.py
@@ -0,0 +1,79 @@
+import os
+import config
+import file_utils
+import cPickle
+
+def make_ngram_strings(fname, n):
+    """
+    Returns list of ngrams (as strings) for a given file.
+    Strings are used to enable dict-based storage.
+    :param fname: name of the file to read words from
+    :param n: the rank of ngrams to create
+    :return: dictionary: {ngram -> num of occurrences}
+    """
+    words = file_utils.get_words_from_file(fname, as_set=False)
+
+    wcount = len(words)
+    ngrams = {}
+
+    for i in xrange(wcount - n + 1):
+        ngram = ' '.join(words[i:i+n])
+
+        if ngram in ngrams:
+            ngrams[ngram] += 1
+        else:
+            ngrams[ngram] = 1
+
+    return ngrams
+
+
+def combine_ngrams_dicts(dict1, dict2):
+    """"
+    Combines two dictionaries with ngrams counts into one
+    :param dict1 First dict
+    :param dict2 Second dict
+    :return dict with combined occurrence counts for ngrams
+    """
+    for ngram, cnt in dict2.iteritems():
+        if ngram in dict1:
+            dict1[ngram] += cnt
+        else:
+            dict1[ngram] = cnt
+
+    return dict1
+
+
+def get_all_ngrams(file_dir, n):
+    """
+    Returns all ngrams from files from a given directory
+    :param file_dir: directory to read files from
+    :param n: rank of ngrams to make
+    :return: string-based dictionary of all ngrams in the directory
+    """
+    files = (os.path.join(file_dir, f) for f in os.listdir(file_dir) if os.path.isfile(os.path.join(file_dir, f)))
+    ngram_dicts = (make_ngram_strings(f, n) for f in files)
+    ngrams_dict = {}
+
+    for ngd in ngram_dicts:
+        ngrams_dict = combine_ngrams_dicts(ngd, ngrams_dict)
+
+    return ngrams_dict
+
+
+def dump_ngrams(ngrams):
+    if not os.path.exists(config.DIR_DUMP):
+        os.makedirs(config.DIR_DUMP)
+
+    dump_file = os.path.join(config.DIR_DUMP, 'dump_edges.pkl')
+    with open(dump_file, 'wb') as f:
+        cPickle.dump(ngrams, f)
+
+
+if __name__ == '__main__':
+    print 'Getting ngrams...'
+    f_dir = os.path.join('..', config.DIR_CLEANED_FILES)
+    ngd = get_all_ngrams(f_dir, 2)
+    # for k, v in ngd.iteritems():
+    #     print "{0}: {1}".format(k, v)
+    dump_ngrams(ngd)
+    print 'Finished dumping ngrams!'
